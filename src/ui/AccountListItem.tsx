f1d5b0a84a42843911d5cb636cf9d5518c21c596
hm <mark.huang1212@gmail.com>
2021-02-06 21:20:08 +0800

    dev UI

--- src/ui/AccountListItem.tsx
+++ src/ui/AccountListItem.tsx
@@ -1,26 +1,26 @@
 interface AccountListItemProps {
-    accountName: string
-    accountType: 'tfc' | 'eth' | 'btc' | 'usdt' | 'bip44'
-    isSelected: boolean
+  accountName: string
+  accountType: 'tfc' | 'eth' | 'btc' | 'usdt' | 'bip44'
+  isSelected: boolean
 }
 
 function AccountListItem(props: AccountListItemProps) {
-    return (
-        <div style={{
-            display: 'flex',
-            padding: '8px',
-            alignItems: 'center'
-        }}>
-            <span style={{ fontSize: '12pt' }}>{props.accountName}</span>
-            <span style={{ flex: 1 }}></span>
-            <span style={{
-                fontSize: '10pt',
-                border: '0.5px solid gray',
-                borderRadius: '4px',
-                padding: '2px'
-            }}>{props.accountType}</span>
-        </div>
-    )
+  return (
+    <div style={{
+      display: 'flex',
+      padding: '8px',
+      alignItems: 'center'
+    }}>
+      <span style={{ fontSize: '12pt' }}>{props.accountName}</span>
+      <span style={{ flex: 1 }}></span>
+      <span style={{
+        fontSize: '10pt',
+        border: '0.5px solid gray',
+        borderRadius: '4px',
+        padding: '2px'
+      }}>{props.accountType}</span>
+    </div>
+  )
 }
 
 export default AccountListItem
