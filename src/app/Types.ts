a2ca03eb1261367df54cc540946a4037786a687c
hm <mark.huang1212@gmail.com>
2021-02-08 09:44:57 +0800

    types

---
+++ src/app/Types.ts
@@ -0,0 +1,26 @@
+interface Coin {
+    index: number,
+    index_hex: string,
+    symbol: string
+    name: string,
+}
+
+interface AccountData {
+    accountId: string,
+    accountType: 'bip44' | 'tfc' | 'eth' | 'btc' | 'erc20',
+    accountName: string,
+    accountBalance?: bigint
+
+    coin?: Coin // for non bip-44 account
+    subAccounts?: AccountData[] // for bip-44 account
+
+    passPhrase: string[]
+    privKey: string
+    pubKey: string
+}
+
+interface AppData {
+    accounts: AccountData[]
+}
+
+export { Coin, AccountData, AppData }
