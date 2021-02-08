1d6dc4f76b0ab864365c816f73f24b13b2b1028f
hm <mark.huang1212@gmail.com>
2021-02-08 09:36:45 +0800

    draft

--- src/app/signal_handlers.ts
+++ src/app/signal_handlers.ts
@@ -1,17 +1,38 @@
 import { ipcMain } from 'electron'
+import Coin from '../core/Coin'
 
-ipcMain.handle('get-accounts', async (event, arg) => {
-    
+interface TxInfo {
+    from: string,
+    to: string,
+    amount: bigint
+}
+
+interface AccountData {
+    accountId: string,
+    accountType: 'bip44' | 'tfc' | 'eth' | 'btc' | 'erc20',
+    accountName: string,
+    accountBalance?: bigint
+
+    coin?: Coin // for non bip-44 account
+    subAccounts?: AccountData[] // for bip-44 account
+
+    passPhrase: string[]
+    privKey: string
+    pubKey: string
+}
+
+ipcMain.handle('get-accounts', async () => {
+
 })
 
-ipcMain.handle('add-account', async (event, accountInfo) => {
+ipcMain.handle('add-account', async (event, accountData: AccountData) => {
 
 })
 
-ipcMain.handle('remove-account', async (event, accountId) => {
+ipcMain.handle('remove-account', async (event, accountId: string) => {
 
 })
 
-ipcMain.handle('transfer-coin', async (async, accountId) => {
+ipcMain.handle('transfer-coin', async (async, txInfo: TxInfo) => {
 
 })
