c22feeebf14235cd51aae14c1fc1d720d6ec47a6
hm <mark.huang1212@gmail.com>
2021-02-08 09:31:19 +0800

    signal hanglers (draft)

---
+++ src/app/signal_handlers.ts
@@ -0,0 +1,17 @@
+import { ipcMain } from 'electron'
+
+ipcMain.handle('get-accounts', async (event, arg) => {
+    
+})
+
+ipcMain.handle('add-account', async (event, accountInfo) => {
+
+})
+
+ipcMain.handle('remove-account', async (event, accountId) => {
+
+})
+
+ipcMain.handle('transfer-coin', async (async, accountId) => {
+
+})
