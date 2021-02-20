3db21649646be8acc86fc29536836f08bd78da78
hm <mark.huang1212@gmail.com>
2021-02-20 15:51:28 +0800

    feature: add standalone account

--- src/app/WalletController.ts
+++ src/app/WalletController.ts
@@ -1,9 +1,20 @@
 import { AccountData } from "../Types";
 import demo_data from './demo_data'
+import { app } from 'electron'
+import path from 'path'
 import PersistentStorageController from "./PersistentStorageController";
 import v8 from 'v8'
+import { v4 as uuidv4 } from 'uuid'
+import { Wallet, WalletJSON } from "../core/wallet";
+import { CoinBTC, CoinETH } from "../Const";
+import { CoinCode } from "../core/defines";
 
-const default_wallet_id = 'main-wallet'
+interface WalletInfo {
+    passphrase?: string[]
+    accountIndexes: {
+        [key in 'ETH' | 'BTC' | 'TFC']: number
+    }
+}
 
 /**
  * This class is responsible for providing the interface for getting,
@@ -11,84 +22,145 @@ const default_wallet_id = 'main-wallet'
  */
 class WalletController {
 
+    wallet: Wallet
+    info: WalletInfo
 
-    walletId: string
-    private psc: PersistentStorageController<AccountData[]>
+    private walletPsc: PersistentStorageController<WalletJSON>
+    private infoPsc: PersistentStorageController<WalletInfo>
 
-    static shared = new WalletController(default_wallet_id, PersistentStorageController.shared as any)
-
-    loadWalletSync() {
-        this.psc.loadSync()
-    }
-
-    getAccountById(id: string) {
-        const account = this.psc.getObjectCopy().find(account => account.accountId === id)
-        return v8.deserialize(v8.serialize(account)) as AccountData | undefined
-    }
-
-    addAccountToWallet(account: AccountData) {
-        if (this.psc.object)
-            this.psc.object.push(v8.deserialize(v8.serialize(account)))
-        else
-            this.psc.object = [v8.deserialize(v8.serialize(account))]
-        this.psc.saveSync()
-    }
-
-    renameAccount(accountId: string, newName: string) {
-        if (this.psc.object) {
-            const account = this.psc.object.find(account => account.accountId === accountId)
-            if (account)
-                account.accountName = newName
-        }
-    }
-
-    renameSubAccount(accountId: string, subAccountId: string, newName: string) {
-
-    }
-
-    loadDemoData() {
-        this.psc.object = v8.deserialize(v8.serialize(demo_data))
-        this.psc.saveSync()
-    }
-
-    deleteAccount(accountId: string) {
-        if (this.psc.object) {
-            this.psc.object = this.psc.object.filter(account => account.accountId !== accountId)
-            this.psc.saveSync()
-        }
-    }
-
-    deleteSubAccount(accountId: string, subAccountId: string) {
-
-    }
+    static shared = new WalletController()
 
     async transfer(accountId: string, destination: string, amount: bigint) {
 
     }
 
-    getPassphraseFromPrivKey(privKey: string) {
-        return ['sample', 'pass', 'phrase']
+    loadDemoData() {
+        this.wallet = Wallet.fromSeed('0x123456789')
+        this.walletPsc.object = this.wallet.toJSON()
+        this.walletPsc.saveSync()
+    }
+
+    loadWallet(mnemonic: string[]): void;
+    loadWallet(seed: string): void;
+    loadWallet(arg: any) {
+        if (typeof arg === 'string') {
+            this.wallet = Wallet.fromSeed(arg)
+        } else if (typeof arg === 'object') {
+            arg = (arg as string[]).join(' ')
+            this.wallet = Wallet.fromMnemonic(arg)
+        } else {
+            throw Error('Invalid argument')
+        }
+        this.walletPsc.object = this.wallet.toJSON()
+        this.walletPsc.save()
+    }
+
+    loadStandaloneAccount(coinType: 'ETH' | 'BTC' | 'TFC', privKey: string) {
+        if (coinType === 'BTC') {
+            this.wallet.getCoinWallet(CoinCode.BTC).addStandaloneAccounts(privKey)
+        } else if (coinType === 'ETH') {
+            this.wallet.getCoinWallet(CoinCode.ETH).addStandaloneAccounts(privKey)
+        } else if (coinType === 'TFC') {
+            this.wallet.getCoinWallet(CoinCode.TFC).addStandaloneAccounts(privKey)
+        }
+        this.walletPsc.object = this.wallet.toJSON()
+        this.walletPsc.save()
     }
 
     getAccounts() {
-        return this.psc.getObjectCopy()
+        const data: AccountData[] = []
+
+        if (this.wallet.seed.toString('hex') !== '12345678') {
+            data.push({
+                accountType: 'bip44-master',
+                accountId: uuidv4(),
+                accountName: 'Main Account',
+                subAccounts: [
+                    {
+                        accountId: uuidv4(),
+                        accountName: 'ETH/ERC20',
+                        accountType: 'bip44-sub-account',
+                        accountBalance: '0',
+                        privKey: this.wallet.coinWallets[60].privateKey.toString('hex'),
+                        pubKey: 'public key',
+                        derivationPath: `m/44'/60'/0'/0/0`,
+                        coinType: CoinETH,
+                    }
+                ],
+                privKey: this.wallet.privateKey.toString('hex'),
+                pubKey: 'public key',
+                passPhrase: ['pass phrase']
+            })
+        }
+
+        this.wallet.getCoinWallet(CoinCode.BTC).standaloneAccounts.forEach(account => {
+            data.push({
+                accountId: uuidv4(),
+                accountName: 'Account',
+                accountType: 'plain',
+                accountBalance: '0',
+                privKey: account.privateKey.toString('hex'),
+                pubKey: account.publicKey,
+                passPhrase: ['pass', 'phrase'],
+                coinType: CoinBTC
+            })
+        })
+
+        this.wallet.getCoinWallet(CoinCode.ETH).standaloneAccounts.forEach(account => {
+            data.push({
+                accountId: uuidv4(),
+                accountName: 'Account',
+                accountType: 'plain',
+                accountBalance: '0',
+                privKey: account.privateKey.toString('hex'),
+                pubKey: account.publicKey,
+                passPhrase: ['pass', 'phrase'],
+                coinType: CoinETH
+            })
+        })
+
+        this.wallet.getCoinWallet(CoinCode.TFC).standaloneAccounts.forEach(account => {
+            data.push({
+                accountId: uuidv4(),
+                accountName: 'Account',
+                accountType: 'plain',
+                accountBalance: '0',
+                privKey: account.privateKey.toString('hex'),
+                pubKey: account.publicKey,
+                passPhrase: ['pass', 'phrase'],
+                coinType: CoinBTC
+            })
+        })
+
+        return data
+
     }
 
-    createPlainAccount() {
+    constructor() {
 
-    }
+        this.walletPsc = new PersistentStorageController(path.join(app.getPath('userData'), 'wallet.json'))
+        this.walletPsc.loadSync()
+        if (this.walletPsc.object) {
+            this.wallet = Wallet.fromJSON(this.walletPsc.getObjectCopy()!)!
+        } else {
+            this.wallet = Wallet.fromSeed('0x12345678')
+        }
 
-    createBipAccount() {
+        this.infoPsc = new PersistentStorageController(path.join(app.getPath('userData'), 'info.json'))
+        this.infoPsc.loadSync()
+        if (this.infoPsc.object) {
+            this.info = this.infoPsc.getObjectCopy()!
+        } else {
+            this.info = {
+                passphrase: [],
+                accountIndexes: {
+                    'BTC': 0,
+                    'ETH': 0,
+                    'TFC': 0
+                }
+            }
+        }
 
-    }
-
-    createSubAccount() {
-
-    }
-
-    constructor(walletId: string, psc: PersistentStorageController<AccountData[]>) {
-        this.walletId = walletId
-        this.psc = psc
     }
 
 }
--- src/ui/ImportAccountVIew.tsx
+++ src/ui/ImportAccountVIew.tsx
@@ -1,66 +1,110 @@
 import React, { useState } from 'react';
-import { Dialog, DialogActions, DialogContent, DialogTitle, Button, FormControl } from '@material-ui/core'
+import { Dialog, DialogActions, DialogContent, DialogTitle, Button, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, TextField } from '@material-ui/core'
 
 interface ImportAccountViewProps {
   visible: boolean
   onCancel: () => any
-  onCreateAccount: () => any
-  onImportAccount: () => any
+  onImportAccount: (format: ImportAccountFormat, type: ImportAccountType, coinType: ImportAccountCoinType | undefined, text: string) => any
+}
+
+type ImportAccountFormat = 'mnemonic' | 'seed'
+type ImportAccountType = 'bip44' | 'plain'
+type ImportAccountCoinType = 'ETH' | 'BTC' | 'TFC'
+
+function ImportAccountActionView(props: {
+  onChange: (format: 'mnemonic' | 'seed', type: 'bip44' | 'plain', coinType: 'ETH' | 'BTC' | 'TFC' | undefined, text: string) => any
+}) {
+
+  const [format, setFormat] = useState<'mnemonic' | 'seed'>('mnemonic')
+  const [accountType, setAccountType] = useState<'bip44' | 'plain'>('bip44')
+  const [text, setText] = useState('')
+  const [coinType, setCoinType] = useState<ImportAccountCoinType | ''>('')
+
+  const onChangeFormat = (e: any) => {
+    setFormat(e.target.value)
+    props.onChange(e.target.value, accountType, coinType !== '' ? coinType : undefined, text)
+  }
+
+  const onChangeAccountType = (e: any) => {
+    setAccountType(e.target.value)
+    props.onChange(format, e.target.value, coinType !== '' ? coinType : undefined, text)
+  }
+
+  const onChangeCoinType = (e: any) => {
+    setCoinType(e.target.value)
+    props.onChange(format, accountType, e.target.value !== '' ? e.target.value : undefined, text)
+  }
+
+  const onChangeTextInput = (e: any) => {
+    setText(e.target.value)
+    props.onChange(format, accountType, coinType !== '' ? coinType : undefined, e.target.value)
+  }
+
+  return (
+    <DialogContent style={{ display: 'flex', flexDirection: 'column' }}>
+      <FormControl>
+        <FormLabel>Account Type</FormLabel>
+        <RadioGroup value={accountType} onChange={onChangeAccountType}>
+          <FormControlLabel value="bip44" control={<Radio />} label="BIP44 Account" />
+          <FormControlLabel value="plain" control={<Radio />} label="Standalone Account" />
+        </RadioGroup>
+      </FormControl>
+      <FormControl style={{ display: accountType === 'plain' ? 'block' : 'none' }}>
+        <FormLabel>Coin Type</FormLabel>
+        <RadioGroup value={coinType} onChange={onChangeCoinType}>
+          <FormControlLabel value="ETH" control={<Radio />} label="ETH" />
+          <FormControlLabel value="BTC" control={<Radio />} label="BTC" />
+          <FormControlLabel value="TFC" control={<Radio />} label="TFC" />
+        </RadioGroup>
+      </FormControl>
+      <FormControl>
+        <FormLabel>Import Format</FormLabel>
+        <RadioGroup value={format} onChange={onChangeFormat}>
+          <FormControlLabel value="mnemonic" control={<Radio />} label="Mnemonic" />
+          <FormControlLabel value="seed" control={<Radio />} label="Seed/Private Key" />
+        </RadioGroup>
+      </FormControl>
+      <TextField label={format} value={text} onChange={onChangeTextInput}></TextField>
+    </DialogContent>
+  )
 }
 
 // eslint-disable-next-line require-jsdoc
 function ImportAccountView(props: ImportAccountViewProps) {
 
-  const [action, setAction] = useState<'pending' | 'importing' | 'creating'>('pending')
+  const [setting, setSetting] = useState<{
+    format: ImportAccountFormat,
+    type: ImportAccountType,
+    coinType: ImportAccountCoinType | undefined,
+    text: string
+  }>({
+    format: 'mnemonic',
+    type: 'bip44',
+    coinType: undefined,
+    text: ''
+  })
 
-  const showCreateAccount = () => {
-    setAction('creating')
+  const onChangeSetting = (format: 'mnemonic' | 'seed', type: 'bip44' | 'plain', coinType: 'ETH' | 'BTC' | 'TFC' | undefined, text: string) => {
+    console.log(text)
+    setSetting({
+      format, type, text, coinType
+    })
   }
 
-  const showImportAccount = () => {
-    setAction('importing')
-  }
-
-  if (action === 'pending') {
-    return (
-      <Dialog open={props.visible} maxWidth="sm" fullWidth>
-        <DialogTitle>Import Or Create Account</DialogTitle>
-        <DialogContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <Button variant="contained" onClick={showCreateAccount} color="primary" style={{ width: '196px' }}>Create Account</Button>
-          <Button variant="contained" onClick={showImportAccount} style={{ marginTop: '8px', width: '196px' }} color="primary">Import Account</Button>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={props.onCancel} color="primary">Cancel</Button>
-        </DialogActions>
-      </Dialog>
-    )
-  }
-
-  if (action === 'creating') {
-    return (
-      <Dialog open={props.visible} maxWidth="sm" fullWidth>
-        <DialogTitle>Creating An Account</DialogTitle>
-        <DialogActions>
-          <Button onClick={() => { setAction('pending') }} color="primary">Back</Button>
-          <Button onClick={props.onCreateAccount} color="primary">Create</Button>
-        </DialogActions>
-      </Dialog>
-    )
-  }
-
-  if (action === 'importing') {
-    return (
-      <Dialog open={props.visible} maxWidth="sm" fullWidth>
-        <DialogTitle>Import An Account</DialogTitle>
-        <DialogActions>
-          <Button onClick={() => { setAction('pending') }} color="primary">Back</Button>
-          <Button onClick={props.onImportAccount} color="primary">Import</Button>
-        </DialogActions>
-      </Dialog>
-    )
-  }
-
-  throw Error()
+  return (
+    <Dialog open={props.visible} maxWidth="sm" fullWidth>
+      <DialogTitle>Import Account</DialogTitle>
+      <ImportAccountActionView onChange={onChangeSetting} />
+      <DialogActions>
+        <Button onClick={props.onCancel} color="primary">Back</Button>
+        <Button onClick={() => props.onImportAccount(
+          setting.format,
+          setting.type,
+          setting.coinType,
+          setting.text)} color="primary">Import</Button>
+      </DialogActions>
+    </Dialog>
+  )
 
 }
 
